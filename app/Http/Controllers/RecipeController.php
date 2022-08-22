630ba0b0197f0bb8c815a8941b3bae215f6d8aa7
Sandor Alin <sandor.alin.nicu@gmail.com>
2022-08-22 15:48:36 +0300

    recipes - Adaugare ruta de fetch all recipes for groups given

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    //
+    public function show(Request $request): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(['recipes' => Recipe::whereIn('group_id', $request->get('groups'))->get()], 200);
+    }
 }
